4901c7d6da69164d7abec2325e762514a7c9430a
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-02-23 17:32:57 -0500

    linalg sketch

--- NDTensors/src/lib/FusionTensors/src/linalg.jl
+++ NDTensors/src/lib/FusionTensors/src/linalg.jl
@@ -7,24 +7,24 @@ using NDTensors.FusionTensors: FusionTensor
 function LinearAlgebra.norm(ft::FusionTensor)
   n2 = 0.0
   for i in blocks(ft)
-    n2 += dimension(sector(row_axis(ft)[i], Block(i))) * norm(matrix(ft).blocks[i])^2
+    n2 += dimension(sector(row_axis(ft)[Block(i)])) * norm(matrix(ft)[Block(i)])^2
   end
   return sqrt(n2)
 end
 
-function LinearAlgebra.qr(t::FusionTensor)
-  q, r = block_qr(t.matrix_blocks)
-  mid_axis = TODO
-  qtens = FusionTensor(t.codomain_axes, (mid_axis,), q)
-  rtens = FusionTensor((mid_axis,), t.domain_axes, r)
+function LinearAlgebra.qr(ft::FusionTensor)
+  q, r = block_qr(matrix(ft))
+  mid_axis = nothing  # TODO
+  qtens = FusionTensor(codomain_axes(ft), (mid_axis,), q)
+  rtens = FusionTensor((mid_axis,), domain_axes(ft), r)
   return qtens, rtens
 end
 
-function LinearAlgebra.svd(t::FusionTensor)
-  u, s, v = block_svd(t.matrix_blocks)
-  mid_axis = TODO
-  utens = FusionTensor(t.codomain_axes, (mid_axis,), u)
+function LinearAlgebra.svd(ft::FusionTensor)
+  u, s, v = block_svd(matrix(ft))
+  mid_axis = nothing  # TODO
+  utens = FusionTensor(codomain_axes(ft), (mid_axis,), u)
   stens = FusionTensor((mid_axis,), (mid_axis,), s)  # TODO struct DiagonalBlockMatrix
-  vtens = FusionTensor((mid_axis,), t.domain_axes, v)
+  vtens = FusionTensor((mid_axis,), domain_axes(ft), v)
   return utens, stens, vtens
 end
