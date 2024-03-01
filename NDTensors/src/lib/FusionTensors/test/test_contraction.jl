887ebe1343f7beb42645af41e94e07658845a7de
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-03-01 15:18:09 -0500

    add sanity_checks

--- NDTensors/src/lib/FusionTensors/test/test_contraction.jl
+++ NDTensors/src/lib/FusionTensors/test/test_contraction.jl
@@ -16,20 +16,22 @@ gr1 = GradedAxes.fuse(g1, g2)
 gc1 = GradedAxes.fuse(g3, g4)
 m1 = BlockSparseArray{Float64}(gr1, gc1)
 ft1 = FusionTensor((g1, g2), (g3, g4), m1)
+@test isnothing(sanity_check(ft1))
 
 gr2 = GradedAxes.dual(GradedAxes.fuse(g3, g4))
 gc2 = GradedAxes.dual(g1)
 m2 = BlockSparseArray{Float64}(gr2, gc2)
 ft2 = FusionTensor(GradedAxes.dual.((g3, g4)), (g1,), m2)
+@test isnothing(sanity_check(ft2))
 
 ft3 = ft1 * ft2  # tensor contraction
-@test isnothing(sanity_check(ft1))
+@test isnothing(sanity_check(ft3))
 @test codomain_axes(ft3) === codomain_axes(ft1)
 @test domain_axes(ft3) === domain_axes(ft2)
 
 # test LinearAlgebra.mul! with in-place matrix product
 mul!(ft3, ft1, ft2)
-@test isnothing(sanity_check(ft2))
+@test isnothing(sanity_check(ft3))
 @test codomain_axes(ft3) === codomain_axes(ft1)
 @test domain_axes(ft3) === domain_axes(ft2)
 
@@ -43,5 +45,6 @@ x = TensorAlgebra.contract(ft1, (1, 2, 3, 4), ft2, (3, 4, 5))
 @test length(x) == 2
 @test x[2] == (1, 2, 5)
 ft4 = x[1]
+@test isnothing(sanity_check(ft4))
 @test codomain_axes(ft4) === codomain_axes(ft1)
 @test domain_axes(ft4) === domain_axes(ft2)
--- NDTensors/src/lib/FusionTensors/test/test_linear_algebra.jl
+++ NDTensors/src/lib/FusionTensors/test/test_linear_algebra.jl
@@ -4,20 +4,20 @@ using Test: @test
 using BlockArrays: Block
 
 using NDTensors.BlockSparseArrays: BlockSparseArray
-using NDTensors.FusionTensors: FusionTensor, codomain_axes, domain_axes, sanity_check
-using NDTensors.GradedAxes
+using NDTensors.FusionTensors: FusionTensor, sanity_check
+using NDTensors.GradedAxes: gradedrange, fuse
 using NDTensors.Sectors: U1
-using NDTensors.TensorAlgebra
 
-g1 = GradedAxes.gradedrange([U1(0), U1(1), U1(2)], [1, 2, 3])
-g2 = GradedAxes.gradedrange([U1(0), U1(1), U1(3)], [2, 2, 1])
-g3 = GradedAxes.gradedrange([U1(-1), U1(0), U1(1)], [1, 2, 1])
-g4 = GradedAxes.gradedrange([U1(-1), U1(0), U1(1)], [1, 1, 1])
+g1 = gradedrange([U1(0), U1(1), U1(2)], [1, 2, 3])
+g2 = gradedrange([U1(0), U1(1), U1(3)], [2, 2, 1])
+g3 = gradedrange([U1(-1), U1(0), U1(1)], [1, 2, 1])
+g4 = gradedrange([U1(-1), U1(0), U1(1)], [1, 1, 1])
 
-gr1 = GradedAxes.fuse(g1, g2)
-gc1 = GradedAxes.fuse(g3, g4)
+gr1 = fuse(g1, g2)
+gc1 = fuse(g3, g4)
 m1 = BlockSparseArray{Float64}(gr1, gc1)
 m1[Block(1, 3)] = ones((2, 4))
 ft1 = FusionTensor((g1, g2), (g3, g4), m1)
+@test isnothing(sanity_check(ft1))
 
 @test norm(ft1) ≈ sqrt(8)
