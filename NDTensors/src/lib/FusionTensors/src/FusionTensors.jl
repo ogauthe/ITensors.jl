4fbfc2dae0e74a5e6ebc986e47eed443ec6f9589
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-03-01 10:48:46 -0500

    clear filenames

--- NDTensors/src/lib/FusionTensors/src/FusionTensors.jl
+++ NDTensors/src/lib/FusionTensors/src/FusionTensors.jl
@@ -1,9 +1,9 @@
 module FusionTensors
 include("fusiontensor/fusiontensor.jl")
+include("fusiontensor/base_interface.jl")
 include("fusiontensor/dense.jl")
-include("fusiontensor/fusiontensorbase.jl")
-#include("fusiontensor/fusiontensorblocks.jl")
-include("fusiontensor/linalg.jl")
+include("fusiontensor/linear_algebra_interface.jl")
+include("fusiontensor/tensor_algebra_interface.jl")
 include("permutedims/structural_data.jl")
 include("permutedims/permutedims.jl")
 end
