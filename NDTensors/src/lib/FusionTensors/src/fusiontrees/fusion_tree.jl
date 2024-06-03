f003738a27f5c3d78fe358b044ede911ebb1ae0d
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-06-03 18:34:05 -0400

    split into more functions

--- NDTensors/src/lib/FusionTensors/src/fusiontrees/fusion_tree.jl
+++ NDTensors/src/lib/FusionTensors/src/fusiontrees/fusion_tree.jl
@@ -37,7 +37,7 @@
 # sector but carry a scaling factor sqrt(dim_sec)
 #
 
-###################################  utility tools  ########################################
+# ===================================  Utility tools  ======================================
 # TODO move tuple operations elsewhere
 flatten_nested_tuple(t::Tuple) = merge_tuples(t...)
 function merge_tuples(t1, t2, t3...)
@@ -62,7 +62,7 @@ function f_to_c_perm(iterable_product)
 end
 
 # compute Kronecker product of fusion trees
-# more efficient with iterative construction
+# more efficient with recursive construction
 trees_kron(a, b, c...) = trees_kron(trees_kron(a, b), c...)
 
 function trees_kron(a, b)
@@ -80,6 +80,7 @@ function _tensor_kron(a::AbstractArray{<:Any,N}, b::AbstractArray{<:Any,N}) wher
   return reshape(c, size(a) .* size(b))
 end
 
+# ================================  High level interface  ==================================
 function compress_tree(a::AbstractArray)
   shape_3leg = (prod(size(a)[begin:(end - 2)]), size(a, ndims(a) - 1), size(a, ndims(a)))
   return reshape(a, shape_3leg)
@@ -97,28 +98,29 @@ function decompress_tree(tree::AbstractArray{<:Any,3}, irreps_shape::NTuple{<:An
   return reshape(tree, new_shape)
 end
 
-#################################   High level interface  ##################################
 function get_tree!(
   dic::Dict{NTuple{N,Int},Vector{Array{Float64,3}}},
   it::NTuple{N,Int},
-  sectors_all::NTuple{N,Vector{C}},
-  isdual::NTuple{N,Bool},
+  irreps_vectors::NTuple{N,Vector{C}},
+  irreps_isdual::NTuple{N,Bool},
   allowed_sectors::Vector{C},
 ) where {N,C<:Sectors.AbstractCategory}
   get!(dic, it) do
-    prune_fusion_trees_compressed(getindex.(sectors_all, it), isdual, allowed_sectors)
+    prune_fusion_trees_compressed(
+      getindex.(irreps_vectors, it), irreps_isdual, allowed_sectors
+    )
   end
 end
 
 function get_tree!(
   dic::Dict{NTuple{N,Int},Vector{<:Array{Float64}}},
   it::NTuple{N,Int},
-  sectors_all::NTuple{N,Vector{C}},
-  isdual::NTuple{N,Bool},
+  irreps_vectors::NTuple{N,Vector{C}},
+  irreps_isdual::NTuple{N,Bool},
   allowed_sectors::Vector{C},
 ) where {N,C<:Sectors.AbstractCategory}
   get!(dic, it) do
-    prune_fusion_trees(getindex.(sectors_all, it), isdual, allowed_sectors)
+    prune_fusion_trees(getindex.(irreps_vectors, it), irreps_isdual, allowed_sectors)
   end
 end
 
@@ -164,23 +166,23 @@ function prune_fusion_trees(
   return trees_sector
 end
 
-##################################  low level interface  ###################################
+# ================================  Low level interface  ===================================
 function fusion_trees(::Tuple{}, ::Tuple{})
   return [ones((1, 1))], [Sectors.sector()]
 end
 
 function fusion_trees(
-  irreps::NTuple{N,<:Sectors.CategoryProduct}, isdual::NTuple{N,Bool}
+  nondual_irreps::NTuple{N,<:Sectors.CategoryProduct}, irreps_isdual::NTuple{N,Bool}
 ) where {N}
   # for CategoryProduct, either compute tree(kron( CG tensor for each category))
   # or kron( tree(CG tensor 1 category) for each category).
   # second option allows for easy handling of Abelian groups and should be more efficient
-  category_irreps = Sectors.categories.(irreps)
+  category_irreps = Sectors.categories.(nondual_irreps)
   n_cat = length(first(category_irreps))
 
   # construct fusion tree for each category
   transposed_cats = ntuple(c -> getindex.(category_irreps, c), n_cat)
-  category_trees_irreps = fusion_trees.(transposed_cats, Ref(isdual))
+  category_trees_irreps = fusion_trees.(transposed_cats, Ref(irreps_isdual))
 
   # reconstruct sector for each product tree
   tree_irreps = map(
@@ -200,57 +202,97 @@ function fusion_trees(
 end
 
 function fusion_trees(
-  irreps::NTuple{N,<:Sectors.AbstractCategory}, isdual::NTuple{N,Bool}
+  nondual_irreps::NTuple{N,<:Sectors.AbstractCategory}, irreps_isdual::NTuple{N,Bool}
 ) where {N}
-  irreps_arrow = ntuple(i -> isdual[i] ? GradedAxes.dual(irreps[i]) : irreps[i], N)
-  return fusion_trees(Sectors.SymmetryStyle(first(irreps)), irreps_arrow, isdual)
+  irreps = ntuple(
+    i -> irreps_isdual[i] ? GradedAxes.dual(nondual_irreps[i]) : nondual_irreps[i], N
+  )
+  return fusion_trees(Sectors.SymmetryStyle(first(irreps)), irreps, irreps_isdual)
 end
 
-#######################################  Internals  ########################################
+# =====================================  Internals  ========================================
 
 # fusion tree for an Abelian group is trivial
-# it does not depend on dual once irreps themselves are dualed
-function fusion_trees(::Sectors.AbelianGroup, irreps_arrow::Tuple, ::Tuple)
-  irrep_prod = reduce(⊗, irreps_arrow)
-  return [ones(ntuple(_ -> 1, length(irreps_arrow) + 2))], [irrep_prod]
+# it does not depend on irreps_isdual once irreps themselves are dualed according to it
+function fusion_trees(::Sectors.AbelianGroup, irreps::Tuple, ::Tuple)
+  irrep_prod = reduce(⊗, irreps)
+  return [ones(ntuple(_ -> 1, length(irreps) + 2))], [irrep_prod]
 end
 
-function add_cg_layer(
-  trees::Vector,
-  tree_irreps::Vector,
-  layer_irrep::Sectors.AbstractCategory,
-  isdual_layer::Bool,
+function build_trees(
+  old_tree::Matrix,
+  old_irrep::Sectors.AbstractCategory,
+  level_irrep::Sectors.AbstractCategory,
+  level_isdual::Bool,
+  ndof_sec::Int,
+  sec::Sectors.AbstractCategory,
 )
-  new_irreps = typeof(tree_irreps)()
-  new_trees = typeof(trees)()
-  for (old_tree, old_irrep) in zip(trees, tree_irreps)
-    sh_cg_dof = (size(old_tree, 2), :)
-    rep = GradedAxes.fusion_product(old_irrep, layer_irrep)
-    for (ndof_sec, sec) in zip(BlockArrays.blocklengths(rep), GradedAxes.blocklabels(rep))
-      shnt = (:, Sectors.quantum_dimension(sec))
-      for inner_multiplicity in 1:ndof_sec
-        cgt_layer_dof = clebsch_gordan_tensor(
-          old_irrep, layer_irrep, sec, false, isdual_layer, inner_multiplicity
-        )
-        nt = old_tree * reshape(cgt_layer_dof, sh_cg_dof)
-        nt = reshape(nt, shnt)
-        push!(new_trees, nt)
-        push!(new_irreps, sec)
-      end
-    end
+  sector_trees = Vector{typeof(old_tree)}()
+  for inner_multiplicity in 1:ndof_sec
+    cgt_inner_mult = clebsch_gordan_tensor(
+      old_irrep, level_irrep, sec, false, level_isdual, inner_multiplicity
+    )
+    dim_old_irrep, dim_level_irrep, dim_sec = size(cgt_inner_mult)
+    tree = old_tree * reshape(cgt_inner_mult, (dim_old_irrep, dim_level_irrep * dim_sec))
+    new_tree = reshape(tree, (size(old_tree, 1) * dim_level_irrep, dim_sec))
+    push!(sector_trees, new_tree)
+  end
+  return sector_trees
+end
+
+function build_trees(
+  old_tree::Matrix,
+  old_irrep::Sectors.AbstractCategory,
+  level_irrep::Sectors.AbstractCategory,
+  level_isdual::Bool,
+)
+  new_trees = Vector{typeof(old_tree)}()
+  new_irreps = Vector{typeof(old_irrep)}()
+  rep = GradedAxes.fusion_product(old_irrep, level_irrep)
+  for (ndof_sec, sec) in
+      zip(GradedAxes.unlabel.(BlockArrays.blocklengths(rep)), GradedAxes.blocklabels(rep))
+    sector_trees = build_trees(
+      old_tree, old_irrep, level_irrep, level_isdual, ndof_sec, sec
+    )
+    append!(new_trees, sector_trees)
+    append!(new_irreps, repeat([sec], ndof_sec))
   end
   return new_trees, new_irreps
 end
 
-function compute_thin_trees(irreps_arrow::Tuple, isdual::Tuple)
-  compressed_thin_trees = [ones((1, 1))]
-  unmerged_tree_irreps = [Sectors.trivial(first(irreps_arrow))]
-  for (irrep, b) in zip(irreps_arrow, isdual)
-    compressed_thin_trees, unmerged_tree_irreps = add_cg_layer(
-      compressed_thin_trees, unmerged_tree_irreps, irrep, b
-    )
+function build_trees(
+  trees::Vector, irreps::Vector, level_irrep::Sectors.AbstractCategory, level_isdual::Bool
+)
+  next_level_trees = typeof(trees)()
+  next_level_irreps = typeof(irreps)()
+  for (old_tree, old_irrep) in zip(trees, irreps)
+    new_trees, new_irreps = build_trees(old_tree, old_irrep, level_irrep, level_isdual)
+    append!(next_level_trees, new_trees)
+    append!(next_level_irreps, new_irreps)
   end
-  return compressed_thin_trees, unmerged_tree_irreps
+  return next_level_trees, next_level_irreps
+end
+
+function build_trees(
+  trees::Vector, tree_irreps::Vector, irreps::Tuple, irreps_isdual::Tuple
+)
+  next_level_trees, next_level_irreps = build_trees(
+    trees, tree_irreps, first(irreps), first(irreps_isdual)
+  )
+  return build_trees(
+    next_level_trees, next_level_irreps, irreps[2:end], irreps_isdual[2:end]
+  )
+end
+
+function build_trees(trees::Vector, tree_irreps::Vector, ::Tuple{}, ::Tuple{})
+  return trees, tree_irreps
+end
+
+function compute_thin_trees(irreps::Tuple, irreps_isdual::Tuple)
+  # init from trivial, NOT from first(irreps) to get isdual correct
+  compressed_thin_trees = [ones((1, 1))]
+  unmerged_tree_irreps = [Sectors.trivial(first(irreps))]
+  return build_trees(compressed_thin_trees, unmerged_tree_irreps, irreps, irreps_isdual)
 end
 
 function merge_trees_irrep(
@@ -259,18 +301,27 @@ function merge_trees_irrep(
   indices_irrep = findall(==(irrep), unmerged_tree_irreps)
   thin_trees_irrep = getindex.(Ref(thin_trees), indices_irrep)
   thick_shape = (size(first(thin_trees_irrep))..., length(indices_irrep))
-  return reshape(hcat(thin_trees_irrep...), thick_shape)
+  return reshape(reduce(hcat, thin_trees_irrep), thick_shape)
+end
+
+function merge_trees_irrep(thin_trees::Vector, unmerged_tree_irreps::Vector)
+  # merge trees fusing on the same irrep (simpler + avoids moving data at each tree level)
+  tree_irreps = sort(unique(unmerged_tree_irreps))
+  thick_trees = map(
+    irrep -> merge_trees_irrep(thin_trees, unmerged_tree_irreps, irrep), tree_irreps
+  )
+  return thick_trees, tree_irreps
 end
 
 # isdual information is still needed to define CG tensor
-function fusion_trees(::Sectors.NonAbelianGroup, irreps_arrow::Tuple, isdual::Tuple)
+function fusion_trees(::Sectors.NonAbelianGroup, irreps::Tuple, irreps_isdual::Tuple)
   # compute trees as 1 tree = 1 degree of freedom
-  thin_compressed_trees, unmerged_tree_irreps = compute_thin_trees(irreps_arrow, isdual)
+  thin_compressed_trees, unmerged_tree_irreps = compute_thin_trees(irreps, irreps_isdual)
+  thick_compressed_trees, tree_irreps = merge_trees_irrep(
+    thin_compressed_trees, unmerged_tree_irreps
+  )
 
-  # merge trees fusing on the same irrep (simpler + avoids moving data at each layer)
-  tree_irreps = sort(unique(unmerged_tree_irreps))
-  thick_compressed_trees =
-    merge_trees_irrep.(Ref(thin_compressed_trees), Ref(unmerged_tree_irreps), tree_irreps)
-  thick_trees = decompress_tree.(thick_compressed_trees, Ref(irreps_arrow))
+  irrep_dims = Sectors.quantum_dimension.(irreps)
+  thick_trees = map(tree -> decompress_tree(tree, irrep_dims), thick_compressed_trees)
   return thick_trees, tree_irreps
 end
