da9421beff05f0852f4d0018275ce105abadff2a
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-09-04 17:27:25 -0400

    use continue

--- NDTensors/src/lib/FusionTensors/src/fusiontensor/dense.jl
+++ NDTensors/src/lib/FusionTensors/src/fusiontensor/dense.jl
@@ -167,7 +167,7 @@ function compress_dense_block(
   #       ext1*dim1   ext2*dim2     ext3*dim3      ext4*dim4       ext5*dim5   ext6*dim6
   #
 
-  # each leg of this this dense block can now be opened to form a 2N-dims tensor.
+  # each leg of this this dense outer block can now be opened to form a 2N-dims tensor.
   # note that this 2N-dims form is only defined at the level of the outer block,
   # not for a larger block.
   #
@@ -315,69 +315,67 @@ function fill_matrix_blocks!(
     codomain_block_allowed_sectors = intersect_sectors(
       codomain_block_irreps, allowed_sectors
     )
-    if !isempty(codomain_block_allowed_sectors)
-      codomain_block_trees = prune_fusion_trees_compressed(
-        codomain_block_irreps, codomain_arrows, allowed_sectors
+    isempty(codomain_block_allowed_sectors) && continue
+    codomain_block_trees = prune_fusion_trees_compressed(
+      codomain_block_irreps, codomain_arrows, allowed_sectors
+    )
+
+    # loop for each domain irrep configuration
+    block_shifts_rows = zeros(Int, length(allowed_sectors))
+    for iter_do in Iterators.product(eachindex.(domain_irreps)...)
+      block_allowed_sectors = intersect_sectors(
+        getindex.(domain_irreps, iter_do), codomain_block_allowed_sectors
+      )
+      isempty(block_allowed_sectors) && continue
+      domain_block_trees = get_tree!(
+        domain_trees_cache, iter_do, domain_irreps, domain_arrows, allowed_sectors
+      )
+      compressed_dense_block = compress_dense_block(
+        view(blockarray, BlockArrays.Block(iter_do..., iter_co...)),
+        getindex.(domain_degens, iter_do),
+        getindex.(domain_dims, iter_do),
+        getindex.(codomain_degens, iter_co),
+        getindex.(codomain_dims, iter_co),
       )
 
-      # loop for each domain irrep configuration
-      block_shifts_rows = zeros(Int, length(allowed_sectors))
-      for iter_do in Iterators.product(eachindex.(domain_irreps)...)
-        block_allowed_sectors = intersect_sectors(
-          getindex.(domain_irreps, iter_do), codomain_block_allowed_sectors
+      # loop for each symmetry sector allowed in this configuration
+      for i_sec in findall(in(block_allowed_sectors), allowed_sectors)
+
+        # actual implementation: legs are conveniently merged
+        #
+        #          ----------------dim_sec---------
+        #          |                              |
+        #          |  struct_mult_domain_sec      |  struct_mult_codomain_sec
+        #           \  /                           \  /
+        #            \/                             \/
+        #            /                              /
+        #           |                               |
+        #     dim1*dim2*dim3                 dim4*dim5*dim6
+        #           |                               |
+        #           -------compressed_dense_block----
+        #           |                               |
+        #     ext1*ext2*ext3                 ext4*ext5*ext6
+
+        # contract fusion trees and reshape symmetric block as a matrix
+        # Note: a final permutedims is needed after the last contract
+        # therefore cannot efficiently use contract!(allowed_matrix_blocks[...], ...)
+        # TBD something like permutedims!(reshape(view), sym_block, (1,3,2,4))?
+        sym_block_sec = contract_fusion_trees(
+          compressed_dense_block, domain_block_trees[i_sec], codomain_block_trees[i_sec]
         )
-        if !isempty(block_allowed_sectors)
-          domain_block_trees = get_tree!(
-            domain_trees_cache, iter_do, domain_irreps, domain_arrows, allowed_sectors
-          )
-          compressed_dense_block = compress_dense_block(
-            view(blockarray, BlockArrays.Block(iter_do..., iter_co...)),
-            getindex.(domain_degens, iter_do),
-            getindex.(domain_dims, iter_do),
-            getindex.(codomain_degens, iter_co),
-            getindex.(codomain_dims, iter_co),
-          )
 
-          # loop for each symmetry sector allowed in this configuration
-          for i_sec in findall(in(block_allowed_sectors), allowed_sectors)
-
-            # actual implementation: legs are conveniently merged
-            #
-            #          ----------------dim_sec---------
-            #          |                              |
-            #          |  struct_mult_domain_sec      |  struct_mult_codomain_sec
-            #           \  /                           \  /
-            #            \/                             \/
-            #            /                              /
-            #           |                               |
-            #     dim1*dim2*dim3                 dim4*dim5*dim6
-            #           |                               |
-            #           -------compressed_dense_block----
-            #           |                               |
-            #     ext1*ext2*ext3                 ext4*ext5*ext6
-
-            # contract fusion trees and reshape symmetric block as a matrix
-            # Note: a final permutedims is needed after the last contract
-            # therefore cannot efficiently use contract!(allowed_matrix_blocks[...], ...)
-            # TBD something like permutedims!(reshape(view), sym_block, (1,3,2,4))?
-            sym_block_sec = contract_fusion_trees(
-              compressed_dense_block, domain_block_trees[i_sec], codomain_block_trees[i_sec]
-            )
-
-            # find position and write matrix block
-            r1 = block_shifts_rows[i_sec]
-            r2 = r1 + size(compressed_dense_block, 1) * size(domain_block_trees[i_sec], 3)
-            c1 = block_shifts_columns[i_sec]
-            c2 = c1 + size(compressed_dense_block, 2) * size(codomain_block_trees[i_sec], 3)
-            @views allowed_matrix_blocks[i_sec][(r1 + 1):r2, (c1 + 1):c2] = sym_block_sec
-            block_shifts_rows[i_sec] = r2
-          end
-        end
+        # find position and write matrix block
+        r1 = block_shifts_rows[i_sec]
+        r2 = r1 + size(compressed_dense_block, 1) * size(domain_block_trees[i_sec], 3)
+        c1 = block_shifts_columns[i_sec]
+        c2 = c1 + size(compressed_dense_block, 2) * size(codomain_block_trees[i_sec], 3)
+        @views allowed_matrix_blocks[i_sec][(r1 + 1):r2, (c1 + 1):c2] = sym_block_sec
+        block_shifts_rows[i_sec] = r2
       end
-
-      codomain_block_length = prod(getindex.(codomain_degens, iter_co))
-      block_shifts_columns += codomain_block_length * size.(codomain_block_trees, 3)
     end
+
+    codomain_block_length = prod(getindex.(codomain_degens, iter_co))
+    block_shifts_columns += codomain_block_length * size.(codomain_block_trees, 3)
   end
 end
 
@@ -512,66 +510,64 @@ function fill_blockarray!(
     codomain_block_existing_sectors = intersect_sectors(
       codomain_block_irreps, existing_sectors
     )
-    if !isempty(codomain_block_existing_sectors)
-      codomain_block_trees = prune_fusion_trees_compressed(
-        codomain_block_irreps, codomain_arrows, existing_sectors
+    isempty(codomain_block_existing_sectors) && continue
+    codomain_block_trees = prune_fusion_trees_compressed(
+      codomain_block_irreps, codomain_arrows, existing_sectors
+    )
+    block_shifts_row = zeros(Int, length(existing_sectors))
+    codomain_block_length = prod(getindex.(codomain_degens, iter_co))
+    codomain_block_dims = getindex.(codomain_dims, iter_co)
+
+    # loop for each domain irrep configuration
+    for iter_do in Iterators.product(eachindex.(domain_irreps)...)
+      block_existing_sectors = intersect_sectors(
+        getindex.(domain_irreps, iter_do), codomain_block_existing_sectors
       )
-      block_shifts_row = zeros(Int, length(existing_sectors))
-      codomain_block_length = prod(getindex.(codomain_degens, iter_co))
-      codomain_block_dims = getindex.(codomain_dims, iter_co)
+      isempty(block_existing_sectors) && continue
+      domain_block_trees = get_tree!(
+        domain_trees_cache, iter_do, domain_irreps, domain_arrows, existing_sectors
+      )
+      domain_block_length = prod(getindex.(domain_degens, iter_do))
+      domain_block_dims = getindex.(domain_dims, iter_do)
 
-      # loop for each domain irrep configuration
-      for iter_do in Iterators.product(eachindex.(domain_irreps)...)
-        block_existing_sectors = intersect_sectors(
-          getindex.(domain_irreps, iter_do), codomain_block_existing_sectors
+      #        ----------------compressed_dense_block--------------------
+      #        |                   |                 |                  |
+      #  ext1*ext2*ext3      ext4*ext5*ext6    dim1*dim2*dim3    dim4*dim5*dim6
+      #
+      compressed_dense_block_shape = (
+        domain_block_length,
+        codomain_block_length,
+        prod(domain_block_dims),
+        prod(codomain_block_dims),
+      )
+      compressed_dense_block = zeros(eltype(blockarray), compressed_dense_block_shape)
+
+      # loop for each symmetry sector inside this configuration
+      for i_sec in findall(in(block_existing_sectors), existing_sectors)
+        c1 = block_shifts_column[i_sec]
+        r1 = block_shifts_row[i_sec]
+        r2 = r1 + size(domain_block_trees[i_sec], 3) * domain_block_length
+        c2 = c1 + size(codomain_block_trees[i_sec], 3) * codomain_block_length
+
+        sym_block_sec = view(existing_matrix_blocks[i_sec], (r1 + 1):r2, (c1 + 1):c2)
+        add_sector_block!(
+          compressed_dense_block,
+          sym_block_sec,
+          domain_block_trees[i_sec],
+          codomain_block_trees[i_sec],
         )
-        if !isempty(block_existing_sectors)
-          domain_block_trees = get_tree!(
-            domain_trees_cache, iter_do, domain_irreps, domain_arrows, existing_sectors
-          )
-          domain_block_length = prod(getindex.(domain_degens, iter_do))
-          domain_block_dims = getindex.(domain_dims, iter_do)
 
-          #        ----------------compressed_dense_block--------------------
-          #        |                   |                 |                  |
-          #  ext1*ext2*ext3      ext4*ext5*ext6    dim1*dim2*dim3    dim4*dim5*dim6
-          #
-          compressed_dense_block_shape = (
-            domain_block_length,
-            codomain_block_length,
-            prod(domain_block_dims),
-            prod(codomain_block_dims),
-          )
-          compressed_dense_block = zeros(eltype(blockarray), compressed_dense_block_shape)
-
-          # loop for each symmetry sector inside this configuration
-          for i_sec in findall(in(block_existing_sectors), existing_sectors)
-            c1 = block_shifts_column[i_sec]
-            r1 = block_shifts_row[i_sec]
-            r2 = r1 + size(domain_block_trees[i_sec], 3) * domain_block_length
-            c2 = c1 + size(codomain_block_trees[i_sec], 3) * codomain_block_length
-
-            sym_block_sec = view(existing_matrix_blocks[i_sec], (r1 + 1):r2, (c1 + 1):c2)
-            add_sector_block!(
-              compressed_dense_block,
-              sym_block_sec,
-              domain_block_trees[i_sec],
-              codomain_block_trees[i_sec],
-            )
-
-            block_shifts_row[i_sec] = r2
-          end
-
-          blockarray[BlockArrays.Block(iter_do..., iter_co...)] = decompress_dense_block(
-            compressed_dense_block,
-            getindex.(domain_degens, iter_do),
-            domain_block_dims,
-            getindex.(codomain_degens, iter_co),
-            codomain_block_dims,
-          )
-        end
+        block_shifts_row[i_sec] = r2
       end
-      block_shifts_column += codomain_block_length * size.(codomain_block_trees, 3)
+
+      blockarray[BlockArrays.Block(iter_do..., iter_co...)] = decompress_dense_block(
+        compressed_dense_block,
+        getindex.(domain_degens, iter_do),
+        domain_block_dims,
+        getindex.(codomain_degens, iter_co),
+        codomain_block_dims,
+      )
     end
+    block_shifts_column += codomain_block_length * size.(codomain_block_trees, 3)
   end
 end
